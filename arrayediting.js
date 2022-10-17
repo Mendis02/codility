2a22fd78b61a0f30da83501f3adebb8ffb0386bc
Mendis02 <103174136+Mendis02@users.noreply.github.com>
2022-10-17 17:49:15 +0530

    Add files via upload

---
+++ arrayediting.js
@@ -0,0 +1,13 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A, K) {
+    // write your code in JavaScript (Node.js 14)
+    for (i of range (K) )
+        lastelement = A[A.length -1]
+        A.pop()
+        A.insert(0,lastelement)
+        return A
+
+
+}
