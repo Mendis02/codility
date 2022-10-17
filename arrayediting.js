89ffa89f533f3e9487c137926dc0679b56ce88fd
Thanura <thanuramendis02@gmail.com>
2022-10-17 18:44:49 +0530

    fixed the foor loop

--- arrayediting.js
+++ arrayediting.js
@@ -3,11 +3,14 @@
 
 function solution(A, K) {
     // write your code in JavaScript (Node.js 14)
-    for (i of range (K) )
-        lastelement = A[A.length -1]
+    
+    for (let i=0; i<K;i++ ){
+        let lastelement = A[A.length -1]
         A.pop()
-        A.insert(0,lastelement)
-        return A
-
-
+        A.unshift(lastelement)
+    }
+    return A
 }
+
+let x= solution([1,2,3],2)
+console.log(x)
