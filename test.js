1aec0dd969fae9c758fac5833ac7a1b0004ca9a8
Thanura <thanuramendis02@gmail.com>
2022-10-20 13:17:16 +0530

    object & get key

---
+++ test.js
@@ -0,0 +1,11 @@
+const occurancesCount = {
+    1:2,
+    3:2,
+    7:3,
+    4:6
+}
+for (let i in occurancesCount){
+    if (occurancesCount[i]%2==1){
+        console.log(occurancesCount)
+    }
+}
