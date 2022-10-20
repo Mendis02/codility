d926bf37b87a1eabb5ed32a716c64ee7d547342c
Thanura <thanuramendis02@gmail.com>
2022-10-20 19:10:50 +0530

    10%

--- findmissingelement.js
+++ findmissingelement.js
@@ -1,18 +1,15 @@
 function solution(A) {
     // write your code in JavaScript (Node.js 14)
     A.sort()
-    let x=1
-    let k=A.length
+    let x = 1
+    let k = A.length
 
-    for(let i=0;i<k;i++){
-    if (A[i] != x){
-        return x
-        break
+    for (let i = 0; i < k; i++) {
+        if (A[i] != x) {
+            return x
+            break
+        }
+        x = x + 1
+
     }
-    x=x+1
-    
-}
-
-
-
 }
