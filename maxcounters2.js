20706ab7515c7787de870ddc9a9b6b91de723d64
Thanura <thanuramendis02@gmail.com>
2022-11-13 16:29:23 +0530

    testing

---
+++ maxcounters2.js
@@ -0,0 +1,24 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(N, A) {
+    // write your code in JavaScript (Node.js 14)
+    var arr = []
+    let maxvalue = 0
+
+    for (let i = 0; i < N; i++) {
+        arr[i] = 0
+    }
+    console.log(arr)
+    
+
+
+
+
+
+
+
+
+}
+
+solution(5, [1, 2, 3, 1, 2, 1,8, 5])
