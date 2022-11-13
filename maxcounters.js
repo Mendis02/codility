b3f00ad799198bd4b9e83b400d9cd13b6d9bedb9
Thanura <thanuramendis02@gmail.com>
2022-11-13 16:28:01 +0530

    timeout errors, 66%

--- maxcounters.js
+++ maxcounters.js
@@ -1,36 +1,34 @@
 function solution(N, A) {
     var arr = []
-    let l = A.length
-    let l2 = arr.length
     let maxvalue = 0
 
     for (let i = 0; i < N; i++) {
         arr[i] = 0
     }
-    console.log(arr)
-    console.log(A)
+    // console.log(arr)
+    // console.log(A)
 
-    for (let K = 0; K < l; K++) {
+    for (let K = 0; K < A.length; K++) {
         if (A[K] <= N) {
             arr[A[K] - 1] = arr[A[K] - 1] + 1
             maxvalue = Math.max(...arr)
-            console.log(arr)
+            // console.log(arr)
             // console.log(maxvalue)
         } else {
-            console.log(maxvalue)
+            // console.log(maxvalue)
 
             //this for loop is not working
-            for (let j = 0; j < l2; j++) {
+            for (let j = 0; j < arr.length; j++) {
                 arr[j] = maxvalue
-                console.log(arr[j])
-                console.log(arr)
+                // console.log(arr[j])
+                // console.log(arr)
             }
-            console.log(A[K])
-            console.log(arr)
+            // console.log(A[K])
+            // console.log(arr)
         }
     }
-
     console.log(arr)
     // console.log(maxvalue)
+
 }
-solution(5, [1, 2, 3, 1, 2, 1,8, 5])
+solution(5, [3,4,4,6,1,4,4])
